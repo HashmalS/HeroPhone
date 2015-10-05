11d93d6db77b2b1bb90d6644db24badf964eb1d2
HashmalS <dragoon8370@gmail.com>
2015-10-05 15:34:21 +0300

    Remove extra imports

--- app/src/main/java/com/hashmals/herophone/CallActivity.java
+++ app/src/main/java/com/hashmals/herophone/CallActivity.java
@@ -1,18 +1,8 @@
 package com.hashmals.herophone;
 
-import android.app.WallpaperManager;
-import android.graphics.drawable.Drawable;
-import android.os.Build;
 import android.support.v4.app.FragmentActivity;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
 import android.view.WindowManager;
-import android.widget.TextView;
 
 public class CallActivity extends FragmentActivity {
 
